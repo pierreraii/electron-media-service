bd012b5312a9e69f34f1d31ea022e9c79a67c259
Pierre Raii <pierresraii@gmail.com>
2020-09-14 17:53:38 +0300

    new build

--- build/Release/.deps/Release/obj.target/electron_media_service/src/darwin/module.o.d
+++ build/Release/.deps/Release/obj.target/electron_media_service/src/darwin/module.o.d
@@ -1,29 +1,33 @@
-cmd_Release/obj.target/electron_media_service/src/darwin/module.o := c++ '-DNODE_GYP_MODULE_NAME=electron_media_service' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DNAPI_DISABLE_CPP_EXCEPTIONS' '-DBUILDING_NODE_EXTENSION' -I/Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node -I/Users/pierreraii/Library/Caches/node-gyp/10.16.0/src -I/Users/pierreraii/Library/Caches/node-gyp/10.16.0/deps/openssl/config -I/Users/pierreraii/Library/Caches/node-gyp/10.16.0/deps/openssl/openssl/include -I/Users/pierreraii/Library/Caches/node-gyp/10.16.0/deps/uv/include -I/Users/pierreraii/Library/Caches/node-gyp/10.16.0/deps/zlib -I/Users/pierreraii/Library/Caches/node-gyp/10.16.0/deps/v8/include -I/Users/pierreraii/Sites/OSS/electron-media-service/node_modules/node-addon-api  -Os -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing  -MMD -MF ./Release/.deps/Release/obj.target/electron_media_service/src/darwin/module.o.d.raw -c -o Release/obj.target/electron_media_service/src/darwin/module.o ../src/darwin/module.mm
+cmd_Release/obj.target/electron_media_service/src/darwin/module.o := c++ '-DNODE_GYP_MODULE_NAME=electron_media_service' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DNAPI_DISABLE_CPP_EXCEPTIONS' '-DBUILDING_NODE_EXTENSION' -I/Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node -I/Users/pierreraii/Library/Caches/node-gyp/12.16.1/src -I/Users/pierreraii/Library/Caches/node-gyp/12.16.1/deps/openssl/config -I/Users/pierreraii/Library/Caches/node-gyp/12.16.1/deps/openssl/openssl/include -I/Users/pierreraii/Library/Caches/node-gyp/12.16.1/deps/uv/include -I/Users/pierreraii/Library/Caches/node-gyp/12.16.1/deps/zlib -I/Users/pierreraii/Library/Caches/node-gyp/12.16.1/deps/v8/include -I/Users/pierreraii/Sites/OSS/electron-media-service/node_modules/node-addon-api  -Os -gdwarf-2 -mmacosx-version-min=10.10 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing  -MMD -MF ./Release/.deps/Release/obj.target/electron_media_service/src/darwin/module.o.d.raw -c -o Release/obj.target/electron_media_service/src/darwin/module.o ../src/darwin/module.mm
 Release/obj.target/electron_media_service/src/darwin/module.o: \
   ../src/darwin/module.mm ../src/darwin/module.h \
   /Users/pierreraii/Sites/OSS/electron-media-service/node_modules/node-addon-api/napi.h \
-  /Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node/node_api.h \
-  /Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node/node_api_types.h \
+  /Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/node_api.h \
+  /Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/js_native_api.h \
+  /Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/js_native_api_types.h \
+  /Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/node_api_types.h \
   /Users/pierreraii/Sites/OSS/electron-media-service/node_modules/node-addon-api/napi-inl.h \
   /Users/pierreraii/Sites/OSS/electron-media-service/node_modules/node-addon-api/napi-inl.deprecated.h \
-  /Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node/uv.h \
-  /Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node/uv/errno.h \
-  /Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node/uv/version.h \
-  /Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node/uv/unix.h \
-  /Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node/uv/threadpool.h \
-  /Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node/uv/darwin.h \
+  /Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/uv.h \
+  /Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/uv/errno.h \
+  /Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/uv/version.h \
+  /Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/uv/unix.h \
+  /Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/uv/threadpool.h \
+  /Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/uv/darwin.h \
   ../src/darwin/service.h
 ../src/darwin/module.mm:
 ../src/darwin/module.h:
 /Users/pierreraii/Sites/OSS/electron-media-service/node_modules/node-addon-api/napi.h:
-/Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node/node_api.h:
-/Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node/node_api_types.h:
+/Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/node_api.h:
+/Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/js_native_api.h:
+/Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/js_native_api_types.h:
+/Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/node_api_types.h:
 /Users/pierreraii/Sites/OSS/electron-media-service/node_modules/node-addon-api/napi-inl.h:
 /Users/pierreraii/Sites/OSS/electron-media-service/node_modules/node-addon-api/napi-inl.deprecated.h:
-/Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node/uv.h:
-/Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node/uv/errno.h:
-/Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node/uv/version.h:
-/Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node/uv/unix.h:
-/Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node/uv/threadpool.h:
-/Users/pierreraii/Library/Caches/node-gyp/10.16.0/include/node/uv/darwin.h:
+/Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/uv.h:
+/Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/uv/errno.h:
+/Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/uv/version.h:
+/Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/uv/unix.h:
+/Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/uv/threadpool.h:
+/Users/pierreraii/Library/Caches/node-gyp/12.16.1/include/node/uv/darwin.h:
 ../src/darwin/service.h:
